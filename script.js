3cab6ff6b8a058f0230fb2b0a0bc316f34de6057
Sean <seanysean700@gmail.com>
2017-09-28 15:39:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 var box = document.getElementsByClassName("box")[0];
 var button = document.getElementById("test");
+var on = false;
 button.addEventListener("click",function() {
-  sessionStorage.setItem("color", "#888");
+  if (!on) {
+    sessionStorage.setItem("color", "#888");
+    on = true;
+  }
+  else {
+    sessionStorage.setItem("color", "#000");
+    on = false;
+  }
 });
 setInterval(function() {
   box.style.background = sessionStorage.getItem("color");
